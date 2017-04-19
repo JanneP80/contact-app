a2b6d3734756e39df9520b06260f770ce5fa4749
Janne Puolitaival <janne.puolitaival@gmail.com>
2017-04-19 19:07:04 +0300

    updated login page

--- client/src/app/contact/user/login/login.component.ts
+++ client/src/app/contact/user/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  userName: string;
+  password: string;
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  loginOK() {
+    this.router.navigate(['/contacts']);
+  }
 }
